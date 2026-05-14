70f97fcdb66a042abac6db0787401da990809e7f
Arthur Heiji Voos Shiroshima <arthurshiro123@gmail.com>
2026-05-14 20:13:18 -0300

    chore: Inicialização do projeto Spring

---
+++ budgie-backend/src/main/java/br/com/shiroshima/budgiebackend/BudgieBackendApplication.java
@@ -0,0 +1,13 @@
+package br.com.shiroshima.budgiebackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BudgieBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BudgieBackendApplication.class, args);
+    }
+
+}
