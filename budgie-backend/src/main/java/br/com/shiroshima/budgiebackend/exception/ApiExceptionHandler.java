e1af36490b09fc95058f98f57b71ddbab071e512
Arthur Heiji Voos Shiroshima <arthurshiro123@gmail.com>
2026-06-09 11:32:56 -0300

    feat(exceptions): Adicionado controller advice para tratamento de exceções, com especificações da documentação e um tratamento padrão

---
+++ budgie-backend/src/main/java/br/com/shiroshima/budgiebackend/exception/ApiExceptionHandler.java
@@ -0,0 +1,70 @@
+package br.com.shiroshima.budgiebackend.exception;
+
+import java.nio.file.AccessDeniedException;
+
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+
+@RestControllerAdvice
+public class ApiExceptionHandler {
+
+    @ExceptionHandler(Exception.class)
+    public ResponseEntity<ErrorMessage> handleUnhandledException(Exception ex) {
+        ErrorMessage e = new ErrorMessage(
+            HttpStatus.INTERNAL_SERVER_ERROR, 
+            "Erro inesperado! Contate o administrador"
+        );
+        return ResponseEntity.status(e.getStatus()).body(e);
+    }
+
+    @ExceptionHandler(ResourceNotFoundException.class)
+    public ResponseEntity<ErrorMessage> handleResourceNotFoundException(ResourceNotFoundException ex) {
+        ErrorMessage e = new ErrorMessage(
+            HttpStatus.INTERNAL_SERVER_ERROR, 
+            "Erro inesperado! Contate o administrador"
+        );
+        return ResponseEntity.status(e.getStatus()).body(e);
+    }
+
+    @ExceptionHandler(BusinessException.class)
+    public ResponseEntity<ErrorMessage> handleBusinessException(BusinessException ex) {
+        ErrorMessage e = new ErrorMessage(
+            HttpStatus.UNPROCESSABLE_CONTENT, 
+            ex.getMessage()
+        );
+        return ResponseEntity.status(e.getStatus()).body(e);
+    }
+
+    @ExceptionHandler(AccessDeniedException.class)
+    public ResponseEntity<ErrorMessage> handleAccessDeniedException(AccessDeniedException ex) {
+        ErrorMessage e = new ErrorMessage(
+            HttpStatus.FORBIDDEN, 
+            "Acesso negado. Verifique se está autenticado e tente novamente"
+        );
+        return ResponseEntity.status(e.getStatus()).body(e);
+    }
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<ErrorMessage> handleMethodArgumentNotValidException(MethodArgumentNotValidException ex) {
+        ErrorMessage e = new ErrorMessage(
+            HttpStatus.BAD_REQUEST, 
+            ex.getMessage()
+        );
+        return ResponseEntity.status(e.getStatus()).body(e);
+    }
+
+    @ExceptionHandler(DataIntegrityViolationException.class)
+    public ResponseEntity<ErrorMessage> handleDataIntegrityViolationException(DataIntegrityViolationException ex) {
+        ErrorMessage e = new ErrorMessage(
+            HttpStatus.CONFLICT, 
+            ex.getMessage()
+        );
+        return ResponseEntity.status(e.getStatus()).body(e);
+    }
+
+}
