c15b21aaed393683298ccc414c0bbaa150b875c7
Arthur Heiji Voos Shiroshima <arthurshiro123@gmail.com>
2026-06-09 11:31:52 -0300

    feat(exceptions): Adicionada classe para mensagem de erro

---
+++ budgie-backend/src/main/java/br/com/shiroshima/budgiebackend/exception/ErrorMessage.java
@@ -0,0 +1,24 @@
+package br.com.shiroshima.budgiebackend.exception;
+
+import java.time.LocalDateTime;
+
+import org.springframework.http.HttpStatus;
+
+import lombok.Getter;
+
+@Getter
+public class ErrorMessage {
+    // { "timestamp": "2025-01-01T10:00:00", "status": 404, "error": "Not Found", "message": "Usuário não encontrado" }
+
+    private LocalDateTime timestamp;
+    private int status;
+    private String error;
+    private String message;
+
+    public ErrorMessage(HttpStatus status, String message) {
+        this.timestamp = LocalDateTime.now();
+        this.status = status.value();
+        this.error = status.getReasonPhrase();
+        this.message = message;
+    }
+}
