4bbe4b9bdacff2953b38380fab17faf752939989
Arthur Heiji Voos Shiroshima <arthurshiro123@gmail.com>
2026-06-09 07:52:15 -0300

    feat(model): Adicionados atributos restantes para o sistema

--- budgie-backend/src/main/java/br/com/shiroshima/budgiebackend/model/User.java
+++ budgie-backend/src/main/java/br/com/shiroshima/budgiebackend/model/User.java
@@ -1,10 +1,14 @@
 package br.com.shiroshima.budgiebackend.model;
 
-import java.util.UUID;
+import java.time.LocalDateTime;
+
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
 
 import jakarta.persistence.*;
+import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Size;
 import lombok.Data;
@@ -14,17 +18,29 @@ import lombok.Data;
 @Data
 public class User {
     @Id
-    @GeneratedValue(strategy = GenerationType.UUID)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(nullable = false)
-    private UUID id;
+    private Long id;
 
-    @NotBlank
-    @Size(min = 2, max = 25, message = "Username should have at least 2 characters and at mos 25 characters")
-    private String username;
+    @NotBlank(message = "Name should not be blank")
+    @Size(min = 2, max = 64, message = "Name should have at least 2 characters and at mos 25 characters")
+    private String name;
 
-    @NotBlank
+    @NotBlank(message = "Email should not be blank")
+    @Email(message = "Must be a valid email")
+    @Column(unique = true)
+    private String email;
+
+    @NotBlank(message = "Password should not be blank")
     @JsonProperty(access = JsonProperty.Access.WRITE_ONLY)
     @Size(min=8, message = "Password should have at least 8 characters")
     private String password;
 
+    @CreatedDate
+    @Column(name = "created_at")
+    private LocalDateTime createdAt;
+
+    @LastModifiedDate
+    @Column(name = "updated_at")
+    private LocalDateTime updatedAt;
 }
