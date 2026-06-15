eabcb9f14e82a8240de6d670aa62d68e63565aec
Arthur Heiji Voos Shiroshima <arthurshiro123@gmail.com>
2026-06-15 10:11:32 -0300

    refactor: Alterado nome do enum renomeado

--- budgie-backend/src/main/java/br/com/shiroshima/budgiebackend/model/WalletMember.java
+++ budgie-backend/src/main/java/br/com/shiroshima/budgiebackend/model/WalletMember.java
@@ -5,7 +5,7 @@ import java.time.LocalDateTime;
 import org.springframework.data.annotation.CreatedDate;
 import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
-import br.com.shiroshima.budgiebackend.model.enums.RoleWallet;
+import br.com.shiroshima.budgiebackend.model.enums.WalletRole;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.EntityListeners;
@@ -41,7 +41,7 @@ public class WalletMember {
 
     @Enumerated(EnumType.STRING)
     @NotNull
-    private RoleWallet role;
+    private WalletRole role;
 
     @CreatedDate
     @Column(name = "created_at")
