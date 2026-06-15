b570591d1c239db71bbbbe396ef80947677ffdbd
Arthur Heiji Voos Shiroshima <arthurshiro123@gmail.com>
2026-06-15 10:19:04 -0300

    feat(config): Classe de configuração de auditoria adicionada

--- budgie-backend/src/main/java/br/com/shiroshima/budgiebackend/config/JpaAuditingConfig.java
+++ budgie-backend/src/main/java/br/com/shiroshima/budgiebackend/config/JpaAuditingConfig.java
@@ -1,7 +1,8 @@
+package br.com.shiroshima.budgiebackend.config;
+
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @Configuration
 @EnableJpaAuditing
-public class JpaAuditingConfig {
-}
+public class JpaAuditingConfig {}
