b1a0ce2818ddecea82ae5bfe633f620e68080b52
Arthur Heiji Voos Shiroshima <arthurshiro123@gmail.com>
2026-06-15 22:07:14 -0300

    fix(userController): Removido método redundante

--- budgie-backend/src/main/java/br/com/shiroshima/budgiebackend/controller/UserController.java
+++ budgie-backend/src/main/java/br/com/shiroshima/budgiebackend/controller/UserController.java
@@ -7,7 +7,6 @@ import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -66,12 +65,6 @@ public class UserController {
         } catch (RuntimeException e) {
             return ResponseEntity.notFound().build();
         }
-    }
-
-    @PostMapping
-    public ResponseEntity<User> addUser(@RequestBody @Valid User user) {
-        var response = service.createUser(user);
-        return ResponseEntity.status(HttpStatus.CREATED).body(response);
     }
 
     @DeleteMapping("/{id}")
