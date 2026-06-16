e08d874d700eb41455ec500ce305bdef12373b96
Arthur Heiji Voos Shiroshima <arthurshiro123@gmail.com>
2026-06-15 22:08:42 -0300

    feat(DTO): Adicionadas DTOs para usuário

---
+++ budgie-backend/src/main/java/br/com/shiroshima/budgiebackend/dto/UserResponseDTO.java
@@ -0,0 +1,18 @@
+package br.com.shiroshima.budgiebackend.dto;
+
+import java.time.LocalDateTime;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class UserResponseDTO {
+    private Long id;
+    private String name;
+    private String email;
+    private LocalDateTime createdAt;
+    private String password;
+}
