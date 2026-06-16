97c7cb54455db5a02028116e7692c51834cc48b3
Arthur Heiji Voos Shiroshima <arthurshiro123@gmail.com>
2026-06-15 22:09:18 -0300

    feat(userService): Adicionado serviço para criação de usuário com hash de senha

--- budgie-backend/src/main/java/br/com/shiroshima/budgiebackend/service/UserService.java
+++ budgie-backend/src/main/java/br/com/shiroshima/budgiebackend/service/UserService.java
@@ -3,11 +3,17 @@ package br.com.shiroshima.budgiebackend.service;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 
+import br.com.shiroshima.budgiebackend.dto.RegisterDTO;
+import br.com.shiroshima.budgiebackend.dto.UserResponseDTO;
+import br.com.shiroshima.budgiebackend.exception.BusinessException;
 import br.com.shiroshima.budgiebackend.model.User;
+import br.com.shiroshima.budgiebackend.model.enums.AuthRole;
 // import br.com.shiroshima.budgiebackend.model.dto.UserCreateDTO;
 import br.com.shiroshima.budgiebackend.repository.UserRepository;
+import jakarta.validation.Valid;
 
 @Service
 public class UserService {
@@ -15,8 +21,16 @@ public class UserService {
     @Autowired
     private UserRepository repo;
 
-    public User createUser(User user) {
-        return repo.save(user);
+    public UserResponseDTO createUser(@Valid RegisterDTO data) {
+        if (repo.findByEmail(data.getEmail()) != null) throw new BusinessException("Usuário já existe");
+        if (!data.getPassword().equals(data.getPasswordConfirmation())) throw new BusinessException("Senha não corresponde com a confirmação");
+
+        String encryptedPassword = new BCryptPasswordEncoder().encode(data.getPassword());
+
+        User user = new User(data.getName(), data.getEmail(), encryptedPassword, AuthRole.USER);
+        User responseUser = repo.save(user);
+
+        return new UserResponseDTO(responseUser.getId(), responseUser.getName(), responseUser.getEmail(), responseUser.getCreatedAt(), responseUser.getPassword());
     }
 
     public List<User> listAll() {
