4c34573271d30822ec1ffddd3741cdbecd23814e
Arthur Heiji Voos Shiroshima <arthurshiro123@gmail.com>
2026-07-02 11:52:11 -0300

    feat(routing): Roteamento configurado

--- budgie-frontend/src/App.tsx
+++ budgie-frontend/src/App.tsx
@@ -1,9 +1,20 @@
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { Signin } from "./pages/Signin";
 import { Login } from "./pages/Login";
 
 function App() {
+  // TODO orquestrar páginas e adicionar fluxo
   return (
-    <Login />
-  );
+    <BrowserRouter>
+        <Routes>
+            <Route path="/login" element={<Login />} />
+            <Route path="/register" element={<Signin />} />
+            <Route path="/recuperar-senha" element={<div />} />
+            <Route path="/app/perfil/senha" element={<div />} />
+            <Route path="/app/dashboard" element={<div />} />
+        </Routes>
+    </BrowserRouter>
+);
 }
 
 export default App;
