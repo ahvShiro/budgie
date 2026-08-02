88f59120e9a31cbcd50ab55f97ba036199d01e12
Arthur Heiji Voos Shiroshima <arthurshiro123@gmail.com>
2026-08-02 16:05:27 -0300

    refactor: Removido @Repository desnecessário

--- budgie-backend/src/main/java/br/com/shiroshima/budgiebackend/repositories/UserRepository.java
+++ budgie-backend/src/main/java/br/com/shiroshima/budgiebackend/repositories/UserRepository.java
@@ -2,11 +2,9 @@ package br.com.shiroshima.budgiebackend.repositories;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.stereotype.Repository;
 
 import br.com.shiroshima.budgiebackend.models.User;
 
-@Repository
 public interface UserRepository extends JpaRepository<User, Long> {
     UserDetails findByEmail(String email);
 }
