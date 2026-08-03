68ab8e00d7795520721e5658781210c0806478e9
Arthur Heiji Voos Shiroshima <arthurshiro123@gmail.com>
2026-08-03 16:12:19 -0300

    refactor: Query JPQL removida

--- budgie-backend/src/main/java/br/com/shiroshima/budgiebackend/repositories/PasswordRedefinitionTokenRepository.java
+++ budgie-backend/src/main/java/br/com/shiroshima/budgiebackend/repositories/PasswordRedefinitionTokenRepository.java
@@ -1,15 +1,15 @@
 package br.com.shiroshima.budgiebackend.repositories;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.query.Param;
 
 import br.com.shiroshima.budgiebackend.models.PasswordRedefinitionToken;
 import br.com.shiroshima.budgiebackend.models.User;
 
 public interface PasswordRedefinitionTokenRepository extends JpaRepository<PasswordRedefinitionToken, Long>{
-    @Query("SELECT t FROM PasswordRedefinitionToken t WHERE t.user = :user ORDER BY t.expiresAt DESC")
-    List<PasswordRedefinitionToken> findTokensByUser(@Param("user") User user);
+    Optional<PasswordRedefinitionToken> findByToken(String token);
+
+    List<PasswordRedefinitionToken> findByUserAndWasAlreadyUsedFalse(User user);
 }
