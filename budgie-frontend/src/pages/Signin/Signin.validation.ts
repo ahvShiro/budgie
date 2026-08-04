f8fa6021fd03e3111451285dd2f762e9c1a38d14
Arthur Heiji Voos Shiroshima <arthurshiro123@gmail.com>
2026-08-04 13:12:31 -0300

    refactor: Usando o schema de password em passwordSchema

--- budgie-frontend/src/pages/Signin/Signin.validation.ts
+++ budgie-frontend/src/pages/Signin/Signin.validation.ts
@@ -1,6 +1,5 @@
+import { passwordSchema } from "@/lib/passwordSchema";
 import { z } from "zod";
-
-const STRONG_PASSWORD_REGEX = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[^\w\s]).+$/;
 
 export const signinSchema = z
   .object({
@@ -14,18 +13,8 @@ export const signinSchema = z
     email: z
       .email("Email deve ser válido")
       .trim()
-      .min(1, "Campo email é obrigatório")
-     ,
-
-    password: z
-      .string()
-      .min(1, "Campo senha é obrigatório")
-      .min(8, "Senha deve ter no mínimo 8 caracteres")
-      .regex(
-        STRONG_PASSWORD_REGEX,
-        "Senha deve conter ao menos 1 letra maiúscula, 1 minúscula, 1 número e 1 caractere especial",
-      ),
-
+      .min(1, "Campo email é obrigatório"),
+    password: passwordSchema,
     passwordConfirmation: z.string().min(1, "Confirmação da senha é obrigatória"),
   })
 
