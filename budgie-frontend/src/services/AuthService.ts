232fabfc5915abcfd00d3890006bee4951fae5a5
Arthur Heiji Voos Shiroshima <arthurshiro123@gmail.com>
2026-08-04 09:27:05 -0300

    feat: Comunicação com API para autenticação

--- budgie-frontend/src/services/AuthService.ts
+++ budgie-frontend/src/services/AuthService.ts
@@ -1,5 +1,5 @@
 import api from "@/configs/axiosConfig";
-import type { ApiUserResponseDTO, RegisterDTO } from "@/services/types";
+import type { ApiAuthResponseDTO, ApiUserResponseDTO, AuthDTO, RegisterDTO } from "@/services/types";
 
 class AuthService {
   private endpoint: string = "/api/v1/auth";
@@ -8,6 +8,15 @@ class AuthService {
     const response = await api.post<ApiUserResponseDTO>(`${this.endpoint}/register`, data);
     return response.data;
   }
+
+  async authenticate(data: AuthDTO) {
+    const response = await api.post<ApiAuthResponseDTO>(`${this.endpoint}/login`, data);
+    return response.data;
+  }
+
+  async recoverPassword() {
+  }
+  async resetPassword() {}
 }
 
 export default new AuthService();
