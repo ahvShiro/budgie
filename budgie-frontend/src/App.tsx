a52ddad70412ed97b0e3c99eb0df912e97256371
Arthur Heiji Voos Shiroshima <arthurshiro123@gmail.com>
2026-08-04 11:36:23 -0300

    feat: Página 404 criada

--- budgie-frontend/src/App.tsx
+++ budgie-frontend/src/App.tsx
@@ -4,6 +4,7 @@ import { Login } from "./pages/Login/Login";
 import { PasswordRecover } from "./pages/PasswordRecover";
 import { PasswordReset } from "./pages/PasswordReset";
 import { Toaster } from "sonner";
+import { NotFound } from "./pages/NotFound/NotFound";
 
 function App() {
   // TODO orquestrar páginas e adicionar fluxo
@@ -16,7 +17,9 @@ function App() {
             <Route path="/recuperar-senha" element={<PasswordRecover />} />
             <Route path="/redefinir-senha" element={<PasswordReset />} />
             <Route path="/app/perfil/senha" element={<div />} />
-            <Route path="/app/dashboard" element={<div />} />
+            <Route path="/app/dashboard" element={<div><p>Oiii</p></div>} />
+            <Route path="/suporte" element={<div><p>Oiii</p></div>} />
+            <Route path="*" element={<NotFound />} />
         </Routes>
     </BrowserRouter>
     <Toaster richColors/>
