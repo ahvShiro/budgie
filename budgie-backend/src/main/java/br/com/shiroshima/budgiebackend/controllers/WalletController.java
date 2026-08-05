b64652d95251996797c39e5f3304e9851afd4cbb
Arthur Heiji Voos Shiroshima <arthurshiro123@gmail.com>
2026-08-04 21:28:38 -0300

    feat: Criado o controller do wallet

---
+++ budgie-backend/src/main/java/br/com/shiroshima/budgiebackend/controllers/WalletController.java
@@ -0,0 +1,52 @@
+package br.com.shiroshima.budgiebackend.controllers;
+
+import java.util.List;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import br.com.shiroshima.budgiebackend.dtos.wallet.WalletRegisterDTO;
+import br.com.shiroshima.budgiebackend.dtos.wallet.WalletResponseDTO;
+import br.com.shiroshima.budgiebackend.dtos.wallet.WalletUpdateDTO;
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+
+@RestController
+@RequestMapping("/api/v1/wallet")
+@RequiredArgsConstructor
+public class WalletController {
+
+    @PostMapping
+    public ResponseEntity<WalletResponseDTO> postWallet(@Valid @RequestBody WalletRegisterDTO data) {
+        return ResponseEntity.status(HttpStatus.CREATED).body(null);
+    }
+
+    @GetMapping
+    public ResponseEntity<List<WalletResponseDTO>> getWallets() {
+        return ResponseEntity.ok(null);
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<WalletResponseDTO> getWallet(@PathVariable Long id) {
+        return ResponseEntity.ok(null);
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<WalletResponseDTO> updateWallet(@PathVariable Long id, @Valid @RequestBody WalletUpdateDTO data) {
+        return ResponseEntity.ok(null);
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> removeWallet(@PathVariable Long id) {
+        return ResponseEntity.noContent().build();
+    }
+
+}
---
+++ budgie-backend/src/main/java/br/com/shiroshima/budgiebackend/dtos/wallet/WalletUpdateDTO.java
@@ -0,0 +1,17 @@
+package br.com.shiroshima.budgiebackend.dtos.wallet;
+
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
+
+public record WalletUpdateDTO(
+    @NotBlank(message = "{name.shouldrequired}")
+    @Size(min = 2, max = 64, message = "{name.shouldminmax}")
+    String name,
+
+    @Size(max = 128, message = "{description.shouldmax}")
+    String description,
+
+    @NotNull
+    Boolean active
+) {}
