b5a38be61c05dca8efb6f81aa5766bd6a9039a33
Arthur Heiji Voos Shiroshima <arthurshiro123@gmail.com>
2026-08-04 23:07:35 -0300

    fix: Alterações na entidade de wallet members

--- budgie-backend/src/main/java/br/com/shiroshima/budgiebackend/models/WalletMember.java
+++ budgie-backend/src/main/java/br/com/shiroshima/budgiebackend/models/WalletMember.java
@@ -11,41 +11,60 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.EntityListeners;
 import jakarta.persistence.EnumType;
 import jakarta.persistence.Enumerated;
+import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
 import jakarta.persistence.Table;
+import jakarta.persistence.UniqueConstraint;
 import jakarta.validation.constraints.NotNull;
-import lombok.Data;
+import lombok.AllArgsConstructor;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 @Entity
-@Table(name = "wallet_members")
-@Data
+@Table(name = "wallet_members", uniqueConstraints = @UniqueConstraint(columnNames = {"wallet_id", "user_id"}))
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@EqualsAndHashCode(of = "id")
 @EntityListeners(AuditingEntityListener.class)
 public class WalletMember {
+
+    public WalletMember(Wallet wallet, User user, WalletRole role)
+    {
+        this.wallet = wallet;
+        this.user = user;
+        this.role = role;
+    }
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(nullable = false)
     private Long id;
-   
-    @ManyToOne
+
+    @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "wallet_id")
+    @NotNull(message = "{wallet.shouldrequired}")
     private Wallet wallet;
 
-    @ManyToOne
+    @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "user_id")
+    @NotNull(message = "{user.shouldrequired}")
     private User user;
 
     @Enumerated(EnumType.STRING)
-    @NotNull
+    @NotNull(message = "{role.shouldrequired}")
     private WalletRole role;
 
     @CreatedDate
     @Column(name = "created_at")
     private LocalDateTime createdAt;
-    
+
     // Você pode adicionar convite pendente, data de expiração etc.
 }
